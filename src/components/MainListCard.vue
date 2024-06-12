e553fe5cf986e996ae3507aa074775c5cb347452
MarcoMpensabene <marcompensabene@gmail.com>
2024-06-12 19:31:22 +0200

    fixed MainSingleCard

--- src/components/MainListCard.vue
+++ src/components/MainListCard.vue
@@ -27,15 +27,7 @@ export default {
             
             <div class="col-12">
                 <div class="row bg-white">
-                    <article v-for="(card , index)  in cardsList" :key="cardsList.id" class="col-2 mb-3" :cardList="cardsList">
-                        <div class="card w-100">
-                            <img :src="card.card_images[0].image_url" class="card-img-top" alt="{{ card.name }}">
-                            <div class="card-body bg-black text-white">
-                                <p class="card-text text-info">{{card.name }}</p>
-                                <p class="card-text text-warning">{{card.type }}</p>
-                            </div>
-                        </div>
-                    </article>
+                    <MainSingleCard v-for="(card , index)  in cardsList" :key="cardsList.id"  :cardsList="card" />
                 </div>
             </div>
         </div>
@@ -44,10 +36,5 @@ export default {
 </template>
 
 <style lang="scss" scoped>
-    p.text-warning {
-        font-size: 10px;
-    }
-    p .text-info{
-        font-size: 12px;
-    }
+
 </style>
--- src/components/MainSingleCard.vue
+++ src/components/MainSingleCard.vue
@@ -7,7 +7,7 @@ export default {
         },
     props :{
         cardsList : {
-            type : Object,
+            type : Array,
             required : true
         }
     }
@@ -15,10 +15,23 @@ export default {
 </script>
 
 <template>
-    
+    <article class="col-2 mb-3">
+    <div class="card w-100">
+        <img :src="cardsList.card_images[0].image_url" class="card-img-top" alt="{{ cardsList.name }}">
+            <div class="card-body bg-black text-white">
+                <p class="card-text text-info">{{cardsList.name }}</p>
+                <p class="card-text text-warning">{{cardsList.type }}</p>
+            </div>
+        </div>
+    </article>
 </template>
 
 <style lang="scss" scoped>
-
+    p.text-warning {
+        font-size: 10px;
+    }
+    p .text-info{
+        font-size: 12px;
+    }
 
 </style>
